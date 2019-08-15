66c3a1f91f76e92f2aa2e86a45ff6b0b82047bac
angsuijin@gmail.com <angsuijin@gmail.com>
2019-08-15 14:56:46 +0800

    Command function

--- gameController.php
+++ gameController.php
@@ -1,5 +1,7 @@
 <?php
 // Command process
+session_start();
+
 $input = strtoupper(strip_tags($_REQUEST['command']));
 
 if(!empty($input)){
@@ -10,7 +12,7 @@ if(!empty($input)){
 			case 'PLACE':
 				$coord = validateCoord($command);
 				if(!$coord){
-					$return['msg'] = 'INVALID DIRECTION';
+					$return['msg'] = error('place');
 				}else{
 					$return['msg'] = place($coord[0], $coord[1], $coord[2]);
 				}
@@ -28,11 +30,11 @@ if(!empty($input)){
 				$return['msg'] = report();
 				break;
 			default:
-				$return['msg'] = 'INVALID COMMAND';
+				$return['msg'] = error('command');
 				break;
 		}
 	}else{
-		$return['msg'] = 'INVALID COMMAND';
+		$return['msg'] = error('command');
 	}
 	
 	$return['command'] = $input;
@@ -70,24 +72,113 @@ function validateCoord($command){
 	return false;
 }
 
+// function to validate coordinate within table
+function validateTable($x, $y){
+	return ( $x >= 0 && $x < 5 && $y >= 0 && $y < 5 );
+}
+
 function place($x, $y, $facing){
-	return 'YOU HAD PLACED YOUR ROBOT !';
+	$direction = ['NORTH', 'SOUTH', 'WEST', 'EAST'];
+	// validate x,y within table
+	if( validateTable($x, $y) ){
+		// validate facing direction within selection
+		if( in_array($facing, $direction) ){
+			// set robot position in session
+			$_SESSION['robot'] =  ['x' => $x, 'y' => $y, 'facing' => $facing];
+			$msg = 'YOU HAD PLACED YOUR ROBOT !';
+		}else{
+			$msg = error('direction');
+		}
+	}else{
+		$msg = error('out');
+	}
+	return $msg;
 }
 
+// function to move the robot 1 step forward to it's facing direction
 function move(){
-	return 'YOUR ROBOT HAS MADE A MOVE FORWARD';
+	if(isset($_SESSION['robot'])){
+		$reset = $_SESSION['robot'];
+
+		switch($_SESSION['robot']['facing']){
+			case 'NORTH' : $_SESSION['robot']['y'] += 1;
+			break;
+			case 'SOUTH' : $_SESSION['robot']['y'] -= 1;
+			break;
+			case 'EAST' : $_SESSION['robot']['x'] += 1;
+			break;
+			case 'WEST' : $_SESSION['robot']['x'] -= 1;
+			break;
+			default: error('direction');
+			break;
+		}
+
+		if( !validateTable( $_SESSION['robot']['x'],  $_SESSION['robot']['y']) ){
+			$_SESSION['robot'] = $reset;
+		}
+		return (  $_SESSION['robot'] !== $reset ? 'YOUR ROBOT HAS MADE A MOVE FORWARD' : error('out') );
+	}else{
+		return error('noRobot');
+	}
 }
 
+// function to turn the robot direction to it's left
 function left(){
-	return 'YOUR ROBOT HAS MADE A LEFT TURN';
+	if(isset($_SESSION['robot'])){
+		switch($_SESSION['robot']['facing']){
+			case 'NORTH' : $_SESSION['robot']['facing'] = 'WEST';
+			break;
+			case 'SOUTH' :$_SESSION['robot']['facing'] = 'EAST';
+			break;
+			case 'EAST' : $_SESSION['robot']['facing'] = 'NORTH';
+			break;
+			case 'WEST' : $_SESSION['robot']['facing'] = 'SOUTH';
+			break;
+			default: error('direction');
+			break;
+		}
+		return 'YOUR ROBOT HAS MADE A LEFT TURN';
+	}else{
+		return error('noRobot');
+	}
 }
 
+// function to turn the robot direction to it's right
 function right(){
-	return 'YOUR ROBOT HAS MADE A RIGHT TURN';
+	if(isset($_SESSION['robot'])){
+		switch($_SESSION['robot']['facing']){
+			case 'NORTH' : $_SESSION['robot']['facing'] = 'EAST';
+			break;
+			case 'SOUTH' :$_SESSION['robot']['facing'] = 'WEST';
+			break;
+			case 'EAST' : $_SESSION['robot']['facing'] = 'SOUTH';
+			break;
+			case 'WEST' : $_SESSION['robot']['facing'] = 'NORTH';
+			break;
+			default: error('direction');
+			break;
+		}
+		return 'YOUR ROBOT HAS MADE A RIGHT TURN';
+	}else{
+		return error('noRobot');
+	}
 }
 
 function report(){
-	return 'REPORT ROBOT';
+	return ( isset($_SESSION['robot']) ? implode(', ', $_SESSION['robot']) : error('noRobot') );
+}
+
+// function to set all type of error message
+function error($type = 'default'){
+	$msg = [
+		'command' => 'INVALID COMMAND. THE VALID COMMAND ARE PLACE | MOVE | LEFT | RIGHT | REPORT. PLEASE TRY AGAIN.',
+		'place' => 'INVALID COMMAND. THE VALID OPTION FOR PLACE COMMAND ARE [ PLACE X-COORD,Y-COORD,DIRECTION ]. PLEASE TRY AGAIN.',
+		'direction' => 'INVALID COMMAND. THE VALID DIRECTION ARE NORTH | SOUTH | WEST | EAST. PLEASE TRY AGAIN.',
+		'out' => 'OUT OF TABLE. COMMAND CANCELLED.',
+		'noRobot' => 'NO ROBOT FOUND ON THE TABLE',
+		'default' => 'INVALID ACTION !'
+	];
+	return $msg[$type];
 }
 
 ?>
