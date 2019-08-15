fce23cc49002492d5fee285be7a53835338745fa
angsuijin@gmail.com <angsuijin@gmail.com>
2019-08-15 15:08:31 +0800

    Style like command line

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<!-- Clear previous robot if page refresh -->
+<?php
+	session_start();
+	if (isset($_SESSION['robot'])){
+		session_destroy();
+	}
+?>
 <html>
 	<head>
+		<link rel="stylesheet" href="main.css">
 		<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 		<script type="text/javascript">
 		    $( document ).ready(function() {
@@ -14,6 +22,7 @@
 	    		    		$('.game__board').append('<p>>>> '+data['command']+'</p>');
 	    		    		$('.game__board').append('<p> '+data['msg']+'</p>');
 	    		    		$('#game_command_form')[0].reset();
+	    		    		$("html, body").animate({ scrollTop: $(document).height() }, 1000);
 		    		    },
 		    		    error: function() {
 		    		        alert('We are sorry! Service temporarily unavailable. Please contact support @ angsuijin@gmail.com.');
@@ -26,7 +35,7 @@
 	<body>
 		<div class="game__board"></div>
 		<br />
-		<form id="game_command_form" name="game_command_form" action="#" method="post">
+		<form id="game_command_form" name="game_command_form" class="game__command__form" action="#" method="post">
 			Enter Command: <input type="text" name="command" required/>
 		</form>
 	</body>
